face0d0fda62d97a08e1870655d16d9b89ed8aa9
webPragmatist <chris@webPragmatist.com>
2010-12-06 13:08:00 -0600

    Adding extension required methods.

--- DependencyInjection/PageExtension.php
+++ DependencyInjection/PageExtension.php
@@ -18,5 +18,35 @@ class PageExtension extends Extension {
         $loader = new XmlFileLoader($container, __DIR__ . '/../Resources/config');
         $loader->load('model.xml');
     }
+
+    /**
+     * Returns the namespace to be used for this extension (XML namespace).
+     *
+     * @return string The XML namespace
+     */
+    public function getNamespace() {
+        return 'http://www.symfony-project.org/shemas/dic/symfony';
+    }
+
+    /**
+     * Returns the base path for the XSD files.
+     *
+     * @return string The XSD base path
+     */
+    public function getXsdValidationBasePath() {
+        return null;
+    }
+
+    /**
+     * Returns the recommended alias to use in XML.
+     *
+     * This alias is also the mandatory prefix to use when using YAML.
+     *
+     * @return string The alias
+     */
+    public function getAlias() {
+        return 'page';
+    }
+
 }
 ?>
